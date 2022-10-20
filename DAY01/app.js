1b82a8eecd3d68c933b9f45535686bd56be1d509
deokdeok <nujabes424@naver.com>
2022-10-20 20:05:48 +0900

    22.10-20

--- DAY01/app.js
+++ DAY01/app.js
@@ -2,17 +2,24 @@
 //  const loginInput = loginForm.querySelector("input");
 //  const loginButton = loginForm.querySelector("button");
 
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting"); 
 
- const loginInput = document.querySelector("#login-form input");
- const loginButton = document.querySelector("#login-form button");
 
- function handleLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username)
-
+function onLoginSubmit(event){
+   event.preventDefault();
+   const HIDDEN_CLASSNAME = "hidden"
+   const username = loginInput.value;
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   console.log(username);
+   // greeting.innerText = "Hello "+username + you well;
+   greeting.innerText = `Hello ${username} you well`;
+   greeting.classList.remove(HIDDEN_CLASSNAME)
  }
+
+
+ loginForm.addEventListener("submit",onLoginSubmit);
+
  
  
- 
- 
- loginButton.addEventListener("click",handleLoginBtnClick);
